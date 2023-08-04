abc73e254154017bbdeda5e946f1fc6c4c64da95
bb515 <bb515@cam.ac.uk>
2023-08-04 16:11:03 +0100

    deleted get_loss_fn_tmp method

--- ssa/losses.py
+++ ssa/losses.py
@@ -4,8 +4,7 @@ import jax
 import jax.numpy as jnp
 import jax.random as random
 from ssa.models import utils as mutils
-from diffusionjax.sde import VE, VP
-from diffusionjax.utils import batch_mul, get_score
+from diffusionjax.utils import batch_mul
 from diffusionjax.losses import get_loss, errors
 
 
@@ -45,65 +44,6 @@ def optimization_manager(config):
 
   return optimize_fn
 
-
-# TODO: delete this probably not needed. What is the actual problem? check the shapes going in/out
-def get_loss_tmp(sde, model, train, reduce_mean=True, continuous=True, likelihood_weighting=True, eps=1e-5):
-  """Create a loss function for training with arbirary SDEs.
-
-  Args:
-    sde: An `sde_lib.SDE` object that represents the forward SDE.
-    model: A `flax.linen.Module` object that represents the architecture of the score-based model.
-    train: `True` for training loss and `False` for evaluation loss.
-    reduce_mean: If `True`, average the loss across data dimensions. Otherwise sum the loss across data dimensions.
-    continuous: `True` indicates that the model is defined to take continuous time steps. Otherwise it requires
-      ad-hoc interpolation to take continuous time steps.
-    likelihood_weighting: If `True`, weight the mixture of score matching losses
-      according to https://arxiv.org/abs/2101.09258; otherwise use the weighting recommended in our paper.
-    eps: A `float` number. The smallest time step to sample from.
-
-  Returns:
-    A loss function.
-  """
-  reduce_op = jnp.mean if reduce_mean else lambda *args, **kwargs: 0.5 * jnp.sum(*args, **kwargs)
-
-  def loss_fn(rng, params, states, batch):
-    """Compute the loss function.
-
-    Args:
-      rng: A JAX random state.
-      params: A dictionary that contains trainable parameters of the score-based model.
-      states: A dictionary that contains mutable states of the score-based model.
-      batch: A mini-batch of training data.
-
-    Returns:
-      loss: A scalar that represents the average loss value across the mini-batch.
-      new_model_state: A dictionary that contains the mutated states of the score-based model.
-    """
-
-    score_fn = mutils.get_score_fn(sde, model, params, states, train=train, continuous=continuous, return_state=True)
-    data = batch['image']
-
-    rng, step_rng = random.split(rng)
-    t = random.uniform(step_rng, (data.shape[0],), minval=eps, maxval=sde.T)
-    rng, step_rng = random.split(rng)
-    z = random.normal(step_rng, data.shape)
-    mean, std = sde.marginal_prob(data, t)
-    perturbed_data = mean + batch_mul(std, z)
-    rng, step_rng = random.split(rng)
-    score, new_model_state = score_fn(perturbed_data, t, rng=step_rng)
-
-    if not likelihood_weighting:
-      losses = jnp.square(batch_mul(score, std) + z)
-      losses = reduce_op(losses.reshape((losses.shape[0], -1)), axis=-1)
-    else:
-      g2 = sde.sde(jnp.zeros_like(data), t)[1] ** 2
-      losses = jnp.square(score + batch_mul(z, 1. / std))
-      losses = reduce_op(losses.reshape((losses.shape[0], -1)), axis=-1) * g2
-
-    loss = jnp.mean(losses)
-    return loss, new_model_state
-
-  return loss_fn
 
 def get_sde_loss_fn(sde, model, train, reduce_mean=True, continuous=True, likelihood_weighting=True, eps=1e-5):
   """Create a loss function for training with arbirary SDEs.
@@ -164,7 +104,7 @@ def get_sde_loss_fn(sde, model, train, reduce_mean=True, continuous=True, likeli
   return loss_fn
 
 
-def get_step_fn(sde, solver, model, train, optimize_fn=None, reduce_mean=False, continuous=True, likelihood_weighting=False):
+def get_step_fn(sde, model, train, optimize_fn=None, reduce_mean=False, continuous=True, likelihood_weighting=False):
   """Create a one-step training/evaluation function.
 
   Args:
@@ -181,23 +121,10 @@ def get_step_fn(sde, solver, model, train, optimize_fn=None, reduce_mean=False, 
     A one-step function for training or evaluation.
   """
   if continuous:
-    # TODO: Maybe won't be valid since get_model_fn uses slightly different
-    # loss_fn = get_loss(sde, solver, model, likelihood_weighting=likelihood_weighting, reduce_mean=reduce_mean)  # stateless
-    # TODO: this doesn't work because of some parameter shape that is different.
-    loss_fn = get_loss(sde, solver, model, likelihood_weighting=likelihood_weighting, reduce_mean=reduce_mean)
-    # TODO: but this doesn't involve the solver and skips that step so maybe a hybrid solution is needed
-    # loss_fn = get_sde_loss_fn(sde, model, train, reduce_mean=True, continuous=True, likelihood_weighting=True, eps=1e-5)
-    # loss_fn = get_loss_tmp(sde, solver, model, likelihood_weighting=likelihood_weighting, reduce_mean=reduce_mean)
-
+    loss_fn = get_sde_loss_fn(sde, model, train, reduce_mean=reduce_mean,
+                              continuous=True, likelihood_weighting=likelihood_weighting)
   else:
-    raise NotImplementedError(f"Discrete training for {sde.__class__.__name__} is not supported.")
-    # assert not likelihood_weighting, "Likelihood weighting is not supported for original SMLD/DDPM training."
-    # if isinstance(sde, VE):
-    #   loss_fn = get_smld_loss_fn(sde, model, train, reduce_mean=reduce_mean)
-    # elif isinstance(sde, VP):
-    #   loss_fn = get_ddpm_loss_fn(sde, model, train, reduce_mean=reduce_mean)
-    # else:
-    #   raise ValueError(f"Discrete training for {sde.__class__.__name__} is not recommended.")
+    raise NotImplementedError()
 
   def step_fn(carry_state, batch):
     """Running one step of training or evaluation.
@@ -218,29 +145,24 @@ def get_step_fn(sde, solver, model, train, optimize_fn=None, reduce_mean=False, 
     rng, step_rng = jax.random.split(rng)
     grad_fn = jax.value_and_grad(loss_fn, argnums=1, has_aux=True)
     if train:
-      raise NotImplementedError()
-      # params = state.optimizer.target
-      # states = state.model_state
-      # (loss, new_model_state), grad = grad_fn(step_rng, params, states, batch)
-      # grad = jax.lax.pmean(grad, axis_name='batch')
-      # new_optimizer = optimize_fn(state, grad)
-      # new_params_ema = jax.tree_multimap(
-      #   lambda p_ema, p: p_ema * state.ema_rate + p * (1. - state.ema_rate),
-      #   state.params_ema, new_optimizer.target
-      # )
-      # step = state.step + 1
-      # new_state = state.replace(
-      #   step=step,
-      #   optimizer=new_optimizer,
-      #   model_state=new_model_state,
-      #   params_ema=new_params_ema
-      # )
+      params = state.optimizer.target
+      states = state.model_state
+      (loss, new_model_state), grad = grad_fn(step_rng, params, states, batch)
+      grad = jax.lax.pmean(grad, axis_name='batch')
+      new_optimizer = optimize_fn(state, grad)
+      new_params_ema = jax.tree_multimap(
+        lambda p_ema, p: p_ema * state.ema_rate + p * (1. - state.ema_rate),
+        state.params_ema, new_optimizer.target
+      )
+      step = state.step + 1
+      new_state = state.replace(
+        step=step,
+        optimizer=new_optimizer,
+        model_state=new_model_state,
+        params_ema=new_params_ema
+      )
     else:
-      # loss = loss_fn(state.params_ema, rng, batch)
-      loss = loss_fn(state.params_ema, state.model_state, step_rng, batch)
-      print(loss)
-      assert 0
-      loss = loss_fn(step_rng, state.params_ema, state.model_state, batch)
+      loss, _ = loss_fn(step_rng, state.params_ema, state.model_state, batch)
       new_state = state
 
     loss = jax.lax.pmean(loss, axis_name='batch')
